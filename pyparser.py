46322b2c423a004774d69b0876c8b02f06e3ab93
Steve Clatterbuck <steve.clatterbuck1@gmail.com>
2017-09-20 12:49:47 -0500

    Add files via upload

---
+++ pyparser.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# Import the email modules we'll need
+import re
+from email.parser import Parser
+from email.utils import parseaddr
+
+#  If the e-mail headers are in a file, uncomment this line:
+headers = Parser().parse(open('email.msg', 'r'))
+
+
+#  Or for parsing headers in a string, use:
+#headers = Parser().parsestr('From: <user@example.com>\n'
+#        'To: <someone_else@example.com>\n'
+#        'Subject: Test message\n'
+#        '\n'
+#        'Body would go here\n')
+
+#  Now the header items can be accessed as a dictionary:
+print 'To: %s' % headers['to']
+print 'From: %s' % headers['from']
+print 'Subject: %s' % headers['subject']
+print 'Date: %s' % headers['date']
+print 'Message-id: %s' % headers['message-id']
+
+
+with open("email.msg") as origin_file:
+    for line in origin_file:
+        if "attachment" in line:
+            print line
+
+
+
+# filename = headers['to'].replace(" ", "").replace("<", "").replace(">", "")
+# print filename
+# datafrom = "Sender : %s" % str (headers['to'])
+# file = open(filename, 'w')
+#     file.write(datafrom)
+# file.close
